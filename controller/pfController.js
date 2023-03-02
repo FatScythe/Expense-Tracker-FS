a1022e1b11bdd463ae720670c2c7afdcf8909642
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-03-02 10:28:06 -0300

    Done with updating profile picture with cloudinary

--- controller/pfController.js
+++ controller/pfController.js
@@ -1,5 +1,9 @@
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+const User = require("../model/User");
+
 const changePfp = async (req, res) => {
   if (!req.files) {
     throw new BadRequestError("Please upload an image");
@@ -8,8 +12,27 @@ const changePfp = async (req, res) => {
   if (!profilePic.mimetype.startsWith("image")) {
     throw new BadRequestError("Please provide an image");
   }
+  if (profilePic.size > process.env.MAX_SIZE) {
+    throw new BadRequestError(
+      `Image size must not be larger than ${process.env.MAX_SIZE} MB`
+    );
+  }
 
-  res.status(StatusCodes.ACCEPTED).json(req.files);
+  // Uploads new profile picture : public_id gives a unique path, hence you can't have two images from one user
+  const result = await cloudinary.uploader.upload(profilePic.tempFilePath, {
+    // use_filename: true,
+    folder: "Expense-tracker-app/users",
+    public_id: `${req.user.userId}`,
+  });
+
+  fs.unlinkSync(profilePic.tempFilePath);
+
+  const userImage = await User.findOneAndUpdate(
+    { _id: req.user.userId },
+    { image: result.secure_url }
+  );
+
+  res.status(StatusCodes.ACCEPTED).json({ image: { src: result.secure_url } });
 };
 
 module.exports = { changePfp };
